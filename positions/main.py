da36b7c234e30691f12e7ce0162ab72a49856efa
Elizaveta <ukrop@LAPTOP-T43LJSG0>
2024-05-20 12:15:02 +0300

    restructure data

--- positions/main.py
+++ positions/main.py
@@ -35,14 +35,17 @@ def index():
 
 @app.get("/instruments")
 async def get_instruments_positions():
-
     positions = []
+
     try:
         data = collection.find({})
         for pos in data:
             positions.append({
-                pos["name"]: pos["position"],
-                "time": pos["time"]
+                "name": pos.get("name", "-"),
+                "position_t1": pos.get("position_t1", "-"),
+                "time_t1": pos.get("time_t1"),
+                "position_t2": pos.get("position_t2", "-"),
+                "time_t2": pos.get("time_t2")
                 })
     except Exception as e:
         positions = f"Error: {e}"
@@ -50,33 +53,34 @@ async def get_instruments_positions():
     return positions
 
 
-@app.get("/instruments/{instrument_name}")
-async def get_instrument_position(instrument_name):
+@app.get("/instruments/{terminal}/{instrument_name}")
+async def get_instrument_position(terminal, instrument_name):
     data = collection.find_one({"name": instrument_name})
     if data:
-        return int(data["position"])
+        return int(data[f"position__t{terminal}"])
     return "no such instrument"
 
 
 @app.post("/instruments")
-async def add_position(name, position):
+async def add_position(terminal, name, position):
     now = datetime.datetime.now()
 
     instrument_position = {
         "name": name,
-        "position": int(position),
-        "time": now
+        f"position_t{terminal}": int(position),
+        f"time_t{terminal}": now
     }
 
     if collection.find_one({"name": name}):
         collection.update_one({"name": name},
                               {"$set": {
-                                  "position": position,
-                                  "time": now
-                              }})
+                                  f"position_t{terminal}": position,
+                                  f"time_t{terminal}": now
+                                  }
+                               })
         print("updated ", name)
     else:
         collection.insert_one(instrument_position)
-        print('added new instrument ', name)
+        print("added new instrument ", name)
 
     return instrument_position
